5613f373294dda416b1c0cdfe2fdddc87767b5e5
FWFWLL <ffl@vivaldi.net>
2022-12-02 11:14:11 +0100

    Update project

--- Advent-of-Code-2022/Day-01-Calorie-Counting/src/bin/part-1.rs
+++ Advent-of-Code-2022/Day-01-Calorie-Counting/src/bin/part-1.rs
@@ -2,8 +2,8 @@ use day_01::process_part_1;
 use std::fs;
 
 fn main() {
-    let file = fs::read_to_string("./input.txt")
+    let input = fs::read_to_string("./input.txt")
         .unwrap();
 
-    println!("{}", process_part_1(&file));
+    println!("{}", process_part_1(&input));
 }
--- Advent-of-Code-2022/Day-01-Calorie-Counting/src/bin/part-2.rs
+++ Advent-of-Code-2022/Day-01-Calorie-Counting/src/bin/part-2.rs
@@ -2,8 +2,8 @@ use day_01::process_part_2;
 use std::fs;
 
 fn main() {
-    let file = fs::read_to_string("./input.txt")
+    let input = fs::read_to_string("./input.txt")
         .unwrap();
 
-    println!("{}", process_part_2(&file));
+    println!("{}", process_part_2(&input));
 }
