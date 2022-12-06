1f488b345dccd7a9751b2419961489e3917a3e67
FWFWLL <ffl@vivaldi.net>
2022-12-06 16:59:01 +0100

    Refactor and use nom to parse input

--- Advent-of-Code-2022/Day-05-Supply-Stacks/src/lib.rs
+++ Advent-of-Code-2022/Day-05-Supply-Stacks/src/lib.rs
@@ -1,190 +1,147 @@
-use std::collections::LinkedList;
+use nom::IResult;
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::character::streaming::space1;
+use nom::sequence::{delimited, preceded};
+use nom::character::complete::{self, alpha1, newline, digit1, multispace1};
+use nom::multi::{separated_list1, many1};
 
-pub fn process_part_1(input: &str) -> String {
-    let (supply_stacks, instructions) = input
-        .split_once("\n\n")
-        .unwrap();
+fn krate(input: &str) -> IResult<&str, Option<&str>> {
+    let (input, char) = alt((
+        tag("   "),
+        delimited(
+            complete::char('['),
+            alpha1,
+            complete::char(']')
+        )
+    ))(input)?;
 
-    let num_stacks: usize = supply_stacks
-        .lines()
+    let result = match char {
+        "   " => None,
+        value => Some(value),
+    };
+
+    Ok((input, result))
+}
+
+fn line(input: &str) -> IResult<&str, Vec<Option<&str>>> {
+    let (input, result) = separated_list1(tag(" "), krate)(input)?;
+
+    Ok((input, result))
+}
+
+#[derive(Debug)]
+struct Instruction {
+    number: u32,
+    from: u32,
+    to: u32,
+}
+
+fn instruction(input: &str) -> IResult<&str, Instruction> {
+    let (input, _) = tag("move ")(input)?;
+    let (input, number) = complete::u32(input)?;
+    let (input, _) = tag(" from ")(input)?;
+    let (input, from) = complete::u32(input)?;
+    let (input, _) = tag(" to ")(input)?;
+    let (input, to) = complete::u32(input)?;
+
+    Ok((
+        input,
+        Instruction {
+            number,
+            from: from - 1,
+            to: to - 1
+        },
+    ))
+}
+
+fn crates(input: &str) -> IResult<&str, (Vec<Vec<&str>>, Vec<Instruction>)> {
+    let (input, crates_horizontal) = separated_list1(newline, line)(input)?;
+    let (input, _) = newline(input)?;
+    let (input, _) = many1(preceded(space1, digit1))(input)?;
+    let (input, _) = multispace1(input)?;
+    let (input, instructions) = separated_list1(newline, instruction)(input)?;
+    
+    let mut crates_vertical: Vec<Vec<Option<&str>>> = vec![];
+
+    (0..=crates_horizontal.len()).for_each(|_| {
+            crates_vertical.push(vec![]);
+    });
+
+    crates_horizontal
+        .iter()
         .rev()
-        .take(1)
-        .next()
-        .unwrap()
-        .trim()
-        .split("   ")
-        .last()
-        .unwrap()
-        .parse::<usize>()
-        .unwrap();
-
-    let mut stacks: Vec<LinkedList<char>> = Vec::<LinkedList<char>>::new();
-
-    for _ in 0..num_stacks {
-        stacks.push(LinkedList::new());
-    }
-
-    let _: () = supply_stacks
-        .replace("    [", "[Ø] [")
-        .replace("]    ", "] [Ø]")
-        .lines()
-        .rev()
-        .skip(1)
-        .into_iter()
-        .map(|line| {
-            let binding = line
-                .replace(&['[', ']'], "");
-
-            let crates = binding
-                .split(' ')
-                .enumerate();
-
-            crates.for_each(|(i, cargo)| {
-                if cargo == "Ø" {}
-                else {
-                    stacks[i].push_front(cargo.chars().next().unwrap())
-                }
-            });
-        })
-    .collect();
-
-    instructions
-        .lines()
-        .for_each(|instruction| {
-            let a = instruction
-                .split(" ");
-            
-            let amount: usize = a
-                .clone()
-                .skip(1)
-                .next()
-                .unwrap()
-                .parse()
-                .unwrap();
-
-            let src: usize = a
-                .clone()
-                .skip(3)
-                .next()
-                .unwrap()
-                .parse::<usize>()
-                .unwrap() - 1;
-
-            let dst: usize = a
-                .clone()
-                .skip(5)
-                .next()
-                .unwrap()
-                .parse::<usize>()
-                .unwrap() - 1;
-
-            for _ in 0..amount {
-                let src_char = stacks[src].pop_front().unwrap();
-
-                stacks[dst].push_front(src_char);
-            }
+        .for_each(|crates| {
+            crates
+                .iter()
+                .enumerate()
+                .for_each(|(i, krate)| {
+                    crates_vertical[i].push(krate.clone());
+                });
         });
 
-    let mut result = String::new();
-    for mut list in stacks {
-        result += list.pop_front().unwrap().to_string().as_str();
-    }
+    let crate_stacks: Vec<Vec<&str>> = crates_vertical
+        .iter()
+        .map(|stack| {
+            stack
+                .iter()
+                .filter_map(|value| *value)
+                .collect()
+        })
+        .collect();
+
+    Ok((input, (crate_stacks, instructions)))
+}
+
+pub fn process_part_1(input: &str) -> String {
+    let (_, (mut crate_stacks, instructions)) = crates(input).unwrap();
+
+    instructions
+        .iter()
+        .for_each(|Instruction {number, from, to}| {
+            let len = crate_stacks[*from as usize].len();
+            crate_stacks[*from as usize].drain((len - *number as usize)..)
+                .rev()
+                .collect::<Vec<&str>>()
+                .iter()
+                .for_each(|krate| {
+                    crate_stacks[*to as usize].push(krate);
+                })
+        });
+
+    let result: String = crate_stacks
+        .iter()
+        .map(|stack| match stack.last() {
+            Some(c) => c,
+            None => "",
+        })
+        .collect();
 
     return result.to_string();
 }
 
 pub fn process_part_2(input: &str) -> String {
-    let (supply_stacks, instructions) = input
-        .split_once("\n\n")
-        .unwrap();
-
-    let num_stacks: usize = supply_stacks
-        .lines()
-        .rev()
-        .take(1)
-        .next()
-        .unwrap()
-        .trim()
-        .split("   ")
-        .last()
-        .unwrap()
-        .parse::<usize>()
-        .unwrap();
-
-    let mut stacks: Vec<LinkedList<char>> = Vec::new();
-
-    for _ in 0..num_stacks {
-        stacks.push(LinkedList::new());
-    }
-
-    let _: () = supply_stacks
-        .replace("    [", "[Ø] [")
-        .replace("]    ", "] [Ø]")
-        .lines()
-        .rev()
-        .skip(1)
-        .into_iter()
-        .map(|line| {
-            let binding = line
-                .replace(&['[', ']'], "");
-
-            let crates = binding
-                .split(' ')
-                .enumerate();
-
-            crates.for_each(|(i, cargo)| {
-                if cargo == "Ø" {}
-                else {
-                    stacks[i].push_front(cargo.chars().next().unwrap())
-                }
-            });
-        })
-    .collect();
+    let (_, (mut crate_stacks, instructions)) = crates(input).unwrap();
 
     instructions
-        .lines()
-        .for_each(|instruction| {
-            let a = instruction
-                .split(" ");
-            
-            let amount: usize = a
-                .clone()
-                .skip(1)
-                .next()
-                .unwrap()
-                .parse()
-                .unwrap();
-
-            let src: usize = a
-                .clone()
-                .skip(3)
-                .next()
-                .unwrap()
-                .parse::<usize>()
-                .unwrap() - 1;
-
-            let dst: usize = a
-                .clone()
-                .skip(5)
-                .next()
-                .unwrap()
-                .parse::<usize>()
-                .unwrap() - 1;
-
-            let mut crates : LinkedList<char> = LinkedList::new();
-            for _ in 0..amount {
-                crates.push_front(stacks[src].pop_front().unwrap());
-            }
-
-            for cargo in crates {
-                stacks[dst].push_front(cargo);
-            }
+        .iter()
+        .for_each(|Instruction {number, from, to}| {
+            let len = crate_stacks[*from as usize].len();
+            crate_stacks[*from as usize].drain((len - *number as usize)..)
+                .collect::<Vec<&str>>()
+                .iter()
+                .for_each(|krate| {
+                    crate_stacks[*to as usize].push(krate);
+                })
         });
 
-    let mut result = String::new();
-    for mut list in stacks {
-        result += list.pop_front().unwrap().to_string().as_str();
-    }
+    let result: String = crate_stacks
+        .iter()
+        .map(|stack| match stack.last() {
+            Some(c) => c,
+            None => "",
+        })
+        .collect();
 
     return result.to_string();
 }
